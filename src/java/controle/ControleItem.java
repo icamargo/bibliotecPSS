28a6cca1b4453c46c07aa2206efab4be93166627
Igor Camargo <igor_camargo96@hotmail.com>
2016-07-21 13:31:03 -0300

    Incluso paginas de exibição do cadastro do item

---
+++ src/java/controle/ControleItem.java
@@ -0,0 +1,13 @@
+package controle;
+//@author igor_
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+@ManagedBean (name = "controleItem")
+@SessionScoped
+public class ControleItem {
+    
+    public ControleItem(){
+    }
+}
